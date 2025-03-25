a51f522960b103bc59f8b3fdba0c74f250a6f5ed
benyyyy <beniwalhimanshu91@gmail.com>
2025-03-25 12:57:56 +0530

    middleware error correction

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -7,10 +7,17 @@ import { connectDb } from "@/helper/db";
 
 
 export async function GET(request) {
+
   const authToken = request.cookies.get("authToken")?.value;
-  console.log(authToken);
+ 
+  if(!authToken){
+    return NextResponse.json({
+      message:"user is not loogged in"
+    })
+  }
+
   const data = jwt.verify(authToken, process.env.JWT_KEY);
-  console.log(data);
+
   await connectDb();
   const user = await User.findById(data._id).select("-password");
   //   console.log("test");
